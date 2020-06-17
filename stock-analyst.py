53520be123bcb6d70288f4654d6ff7b3ee1b01eb
Dhairya Surana <dhairs7@vt.edu>
2020-06-16 23:21:36 -0400

    Added feature to list stocks based on upper limit price

--- stock-analyst.py
+++ stock-analyst.py
@@ -1,1 +1,20 @@
 # stock-analyst.py
+
+from yahooquery import Ticker
+import pprint
+from get_all_tickers import get_tickers as gt
+import sys
+
+if __name__ == "__main__":
+
+    list_of_tickers = gt.get_tickers()
+    limit = float(sys.argv[1])
+
+    for ticker in list_of_tickers:
+        company = Ticker(ticker)
+        try:
+            curr_price = company.financial_data[ticker]['currentPrice']
+            if curr_price <= limit:
+                print(ticker, ": $", curr_price)
+        except Exception:
+            print(ticker, " symbol not found")
