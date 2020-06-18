dfd3892fc4fbb37cf2d79cb58b45882b3c3e6ac9
Dhairya Surana <dhairs7@vt.edu>
2020-06-18 19:19:32 -0400

    Got rid of feature in previous commit and switched from yahooquery to selenium

--- stock-analyst.py
+++ stock-analyst.py
@@ -1,20 +1,54 @@
-# stock-analyst.py
+# # stock-analyst.py
 
-from yahooquery import Ticker
-import pprint
-from get_all_tickers import get_tickers as gt
+from bs4 import BeautifulSoup
+from selenium import webdriver 
 import sys
+import re
+import requests
 
-if __name__ == "__main__":
 
-    list_of_tickers = gt.get_tickers()
-    limit = float(sys.argv[1])
+def getData(attr):
 
-    for ticker in list_of_tickers:
-        company = Ticker(ticker)
-        try:
-            curr_price = company.financial_data[ticker]['currentPrice']
-            if curr_price <= limit:
-                print(ticker, ": $", curr_price)
-        except Exception:
-            print(ticker, " symbol not found")
+    stat = soup.find(text=re.compile(attr))
+    data = soup.find(text=re.compile(attr)).find_next('div').contents[0].strip()
+
+    return (stat, data)
+
+
+
+if __name__=="__main__":
+
+    ticker = sys.argv[1]
+    url = f"https://www.morningstar.com/stocks/xnas/{ticker}/quote"
+
+    browser = webdriver.Chrome()
+    # browser.get(url)
+
+    # soup = BeautifulSoup(browser.page_source,'lxml')
+
+    # price = soup.find('div', {'id':'message-box-price'})
+    # price2 = price.text.strip()
+    # print(price2)
+
+    url = f"https://www.morningstar.com/stocks/xnas/{ticker}/financials"
+    browser.get(url)
+    soup = BeautifulSoup(browser.page_source,'lxml')
+    print(getData("Debt/Equity"))
+
+    browser.close()
+    
+    # try:
+    #     r = requests.get(url)
+    #     r.raise_for_status()
+
+    # except requests.exceptions.HTTPError as e:
+    #     raise SystemExit(e) 
+
+    # # Parse the html content
+    # soup = BeautifulSoup(r.text, "lxml")
+    # print(soup.text) # print the parsed data of html
+
+    # # stats = soup.find_all("li")
+    # # for div in stats:
+    # #     print(div)
+  
