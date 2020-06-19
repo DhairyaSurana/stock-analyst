d08a06e0bb7f49fc49facc6c2a836ea7b0e71268
Dhairya Surana <dhairs7@vt.edu>
2020-06-19 01:22:49 -0400

    Prevented chrome browser from opening while program is running

--- stock-analyst.py
+++ stock-analyst.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options  
 import sys
 import re
 import requests
@@ -17,25 +18,25 @@ if __name__=="__main__":
     ticker = sys.argv[1]
     url = f"https://www.morningstar.com/stocks/xnas/{ticker}/quote"
 
-    browser = webdriver.Chrome()
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")   # Allows web access without opening browser
+    chrome_options.add_argument('--log-level=3') # Prevents headless chrome-related console messages from printing
+    browser = webdriver.Chrome(options=chrome_options)
     browser.get(url)
 
     soup = BeautifulSoup(browser.page_source,'lxml')
 
     price = soup.find('div', {'id':'message-box-price'})
     price2 = price.text.strip()
-    print("Price: ", price2)
+   
 
     url = f"https://www.morningstar.com/stocks/xnas/{ticker}/financials"
     browser.get(url)
     soup = BeautifulSoup(browser.page_source,'lxml')
     
-    pe_ratio = getData("Price/Earnings")
-    debt_equ_ratio = getData("Debt/Equity")
-    roic = getData("Invested Capital %")
+    print("Price: ", price2)
+    print("P/E: ", getData("Price/Earnings"))
+    print("Debt/Equity: ", getData("Debt/Equity"))
+    print("ROIC: ", getData("Invested Capital %"))
 
-    print("P/E: ", pe_ratio)
-    print("Debt/Equity: ", debt_equ_ratio)
-    print("ROIC: ", roic)
-
-    browser.close()
+    browser.close()   
