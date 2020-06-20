8fba4998d4cc15fe24d401794b275ddecc9c15be
Dhairya Surana <dhairs7@vt.edu>
2020-06-20 14:13:59 -0400

    Added a ticker list to scrape multiple stocks at once

--- stock-analyst.py
+++ stock-analyst.py
@@ -3,10 +3,12 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver 
 from selenium.webdriver.chrome.options import Options  
+from yahooquery import Ticker
+from statistics import mean
 import sys
 import re
 import requests
-
+import time
 
 def getData(attr):
 
@@ -15,36 +17,53 @@ def getData(attr):
 
 if __name__=="__main__":
 
-    ticker = sys.argv[1]
-    url = f"https://www.morningstar.com/stocks/xnas/{ticker}/quote"
+    ticker_list = ["AAPL", "TSLA", "ZNGA", "CCLP"]
+    dur_list = []
 
-    r = requests.get(url)
+    for ticker in ticker_list:
 
-    try:
-        r.raise_for_status()
-    except Exception as e:
-        raise SystemExit(e)
+        start_time = time.time()
 
-    chrome_options = Options()  
-    chrome_options.add_argument("--headless")   # Allows web access without opening browser
-    chrome_options.add_argument('--log-level=3') # Prevents headless chrome-related console messages from printing
-    browser = webdriver.Chrome(options=chrome_options)
+        #url = f"https://www.morningstar.com/stocks/xnas/{ticker}/financials"
+        url = f"https://www.morningstar.com/stocks/xnas/{ticker.lower()}/quote"
 
-    browser.get(url)
+        r = requests.get(url)
 
-    soup = BeautifulSoup(browser.page_source,'lxml')
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            raise SystemExit(e)
 
-    price = soup.find('div', {'id':'message-box-price'})
-    price2 = price.text.strip()
-   
+        chrome_options = Options()  
+        chrome_options.add_argument("--headless")   # Allows web access without opening browser
+        chrome_options.add_argument('--log-level=3') # Prevents headless chrome-related console messages from printing
+        browser = webdriver.Chrome(options=chrome_options)
 
-    url = f"https://www.morningstar.com/stocks/xnas/{ticker}/financials"
-    browser.get(url)
-    soup = BeautifulSoup(browser.page_source,'lxml')
+        browser.get(url)
+
+        soup = BeautifulSoup(browser.page_source,'lxml')
+
+        price = soup.find('div', {'id':'message-box-price'})
+        price2 = price.text.strip()
     
-    print("Price: ", price2)
-    print("P/E: ", getData("Price/Earnings"))
-    print("Debt/Equity: ", getData("Debt/Equity"))
-    print("ROIC: ", getData("Invested Capital %"))
 
+        url = f"https://www.morningstar.com/stocks/xnas/{ticker}/financials"
+        browser.get(url)
+        soup = BeautifulSoup(browser.page_source,'lxml')
+
+        company = Ticker(ticker)
+        
+        #print("Price: ", company.financial_data[ticker]['currentPrice'])
+        print("Company: ", ticker)
+        print("     Price: ", price2)
+        print("     P/E: ", getData("Price/Earnings"))
+        print("     Debt/Equity: ", getData("Debt/Equity"))
+        print("     ROIC: ", getData("Invested Capital %"))
+
+        duration = round(time.time() - start_time, 2) 
+        dur_list.append(duration)
+
+        print()
+
+    print("Avg Runtime: ", mean(dur_list), " s")
     browser.close()   
