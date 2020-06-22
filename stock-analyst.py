0ebfea067ba40850d8a457cf25de7448f67c9a49
Dhairya Surana <dhairs7@vt.edu>
2020-06-22 12:02:00 -0400

    Reduced avg runtime to 7-8 s

--- stock-analyst.py
+++ stock-analyst.py
@@ -8,15 +8,11 @@ from yahooquery import Ticker
 from statistics import mean
 import sys
 import re
-import requests
 import time
+import os
 
-# def getURL(browser, ticker):
 
-#     browser.find_element_by_class_name("mdc-site-search__form").send_keys(ticker)
-#     return browser.current_url
-
-def getData(attr):
+def getData(soup, attr):
 
     data = soup.find(text=re.compile(attr)).find_next('div').contents[0].strip()
     return data
@@ -26,53 +22,61 @@ if __name__=="__main__":
     ticker_list = ["PCRFY", "MSFT", "AMZN", "AAPL", "TSLA", "ZNGA", "CCLP"]
     dur_list = []
 
-    chrome_options = Options()  
-    #chrome_options.add_argument("--headless")   # Allows web access without opening browser
-    #chrome_options.add_argument('--window-size=1920x1080')
-    #chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    chrome_options.add_argument('--log-level=3') # Prevents headless chrome-related console messages from printing
-    browser = webdriver.Chrome(options=chrome_options)
-
-    url = f"https://www.morningstar.com/stocks"
-    browser.get(url)
-
-    for ticker in ticker_list:
-
-        start_time = time.time()
-
-        # Enter ticker symbol in search bar        
-        browser.find_element_by_tag_name("input").send_keys(ticker)
-        time.sleep(2)
-        browser.find_element_by_tag_name("input").send_keys(Keys.RETURN)
-
-        time.sleep(5)
-        soup = BeautifulSoup(browser.page_source,'lxml')
-
-        # Try to find the price, if not, try again    
-        price = soup.find('div', {'id':'message-box-price'})
-        #price = browser.find_element_by_id('message-box-price')
-        price2 = price.text.strip()
-            
-
+    try:
     
-        # Click on 'Financials' tab
-        browser.find_element_by_link_text("Financials").send_keys(Keys.RETURN)
-        time.sleep(2)
-        soup = BeautifulSoup(browser.page_source,'lxml')
+        chrome_options = Options()  
+        chrome_options.add_argument("--headless")   # Allows web access without opening browser
+        chrome_options.add_argument('--window-size=1920x1080')
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        chrome_options.add_argument('--log-level=3') # Prevents headless chrome-related console messages from printing
+        browser = webdriver.Chrome(options=chrome_options)
 
-        company = Ticker(ticker)
-        
-        print("Company: ", ticker)
-        #print("    Price: ", company.financial_data[ticker]['currentPrice'])
-        print("     Price: ", price2)
-        print("     P/E: ", getData("Price/Earnings"))
-        print("     Debt/Equity: ", getData("Debt/Equity"))
-        print("     ROIC: ", getData("Invested Capital %"))
+        time.sleep(1)
 
-        duration = round(time.time() - start_time, 2) 
-        dur_list.append(duration)
+        url = "https://www.morningstar.com/stocks"
+        browser.get(url)
 
+        for ticker in ticker_list:
+
+            start_time = time.time()
+
+            company = Ticker(ticker)
+            fin_data = company.financial_data[ticker]
+
+            # Enter ticker symbol in search bar        
+            browser.find_element_by_tag_name("input").send_keys(ticker)
+            time.sleep(1)
+            browser.find_element_by_tag_name("input").send_keys(Keys.RETURN)
+
+            time.sleep(2)
+                
+            # Click on 'Financials' tab
+            browser.find_element_by_link_text("Financials").send_keys(Keys.RETURN)
+            time.sleep(2)
+            soup = BeautifulSoup(browser.page_source,'lxml')
+
+            # Print Company Stats
+            print("Company: ", ticker)
+            print("     Price: ", fin_data["currentPrice"])
+            print("     P/E: ", getData(soup, "Price/Earnings"))
+            print("     Debt/Equity: ", round(fin_data["debtToEquity"]/100, 2))
+            print("     ROIC: ", getData(soup, "Invested Capital %"))
+            print()
+
+            duration = round(time.time() - start_time, 2) 
+            dur_list.append(duration)
+
+        print("Avg Runtime/stock: ", mean(dur_list), " s")
+        browser.close()   
+    
+    except Exception as e:
+        print(e)
         print()
+        print("Quitting browser")
+        os.system("taskkill /IM \"chrome.exe\" /F")
 
-    print("Avg Runtime: ", mean(dur_list), " s")
-    browser.close()   
+    except KeyboardInterrupt:
+        print()
+        print("Quitting browser")
+        os.system("taskkill /IM \"chrome.exe\" /F")
+    
