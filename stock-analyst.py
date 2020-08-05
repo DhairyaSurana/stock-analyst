84bc863e2cc4e45fce382ba6513d8b2177702384
Dhairya Surana <dhairs7@vt.edu>
2020-08-04 21:28:21 -0400

    Stock data is printed to an output text file

--- stock-analyst.py
+++ stock-analyst.py
@@ -25,16 +25,33 @@ def getURL(ticker):
     crl.setopt(crl.WRITEDATA, b_obj)
 
     # Perform a file transfer 
-    crl.perform() 
+    conn = 0
+    for i in range(1, 6):
+        try:
+            conn = i
+            crl.perform()
+            break
+        except Exception:
+
+            if(i == 5):
+                SystemExit("Pycurl perform() attempts exceeded")
+            continue 
+
+        
 
     # Get the content stored in the BytesIO object (in byte characters) 
     result =  b_obj.getvalue().decode('utf8')
 
     crl.close()
 
-    time.sleep(0.15)
+    time.sleep(0.3)
 
-    return result
+    # name_start = "<span class=\"companyName\">"
+    # roc_end = "Return on Invested Capital"
+
+    # return result[result.index(name_start) : result.index(roc_end)]
+
+    return (ticker, conn,  result)
 
 
 def getPages(ticker_list):
@@ -45,39 +62,44 @@ def getPages(ticker_list):
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
         return executor.map(getURL, ticker_list)
 
-def getData(html):
+def getData(tup, out_file):
+
+    ticker, conn, html = tup
 
     html_text = re.sub('<[^<]+?>', '', html)
 
     name_start = "<span class=\"companyName\">"
     name_end = "</span> <span class=\"tickerName\">"
+
+    out_file.write("URL: " + f'https://www.wsj.com/market-data/quotes/{ticker}/financials\n')
+    out_file.write(f"Connection attempts: {conn}\n")
     try:
-        print("Company: ", html[html.index(name_start) + len(name_start) : html.index(name_end)])
+        out_file.write("Company: " + html[html.index(name_start) + len(name_start) : html.index(name_end)] + "\n")
     except:
-        print("Company: not found")
+        out_file.write("Company: not found\n")
 
     price_start = "<span id=\"quote_val\">"
     price_end = "</span></span> <span class=\"cr_sym\""
     try:
-        print("     Price: ", html[html.index(price_start) + len(price_start) : html.index(price_end)])
+        out_file.write("     Price: " + html[html.index(price_start) + len(price_start) : html.index(price_end)] + "\n")
     except:
-        print("     Price: not found")
+        out_file.write("     Price: not found\n")
 
     pe_start = "P/E Ratio (TTM)"
     pe_end = "P/E Ratio (including extraordinary items)"
     try:
-        print("     P/E Ratio: ", html_text[html_text.index(pe_start) + len(pe_start) : html_text.index(pe_end)])
+        out_file.write("     P/E Ratio: " + html_text[html_text.index(pe_start) + len(pe_start) : html_text.index(pe_end)] + "\n")
     except:
-        print(      "P/E Ratio: not found")
+        out_file.write(      "P/E Ratio: not found\n")
 
     roc_start = "Return on Total Capital"
     roc_end = "Return on Invested Capital"
     try:
-        print("     ROC: ", html_text[html_text.index(roc_start) + len(roc_start) : html_text.index(roc_end)])
+        out_file.write("     ROC: " + html_text[html_text.index(roc_start) + len(roc_start) : html_text.index(roc_end)] + "\n")
     except:
-        print("     ROC: not found")
+        out_file.write("     ROC: not found\n")
 
-    print()
+    out_file.write("\n")
 
     
 if __name__ == "__main__":
@@ -85,14 +107,22 @@ if __name__ == "__main__":
     start_time = time.time()
     
     try:
-        file = open(sys.argv[1], 'rb')
+        inp_file = open(sys.argv[1], 'rb')
+        out_file = open("data.txt", 'a')
         
     except Exception as e:
         SystemExit(e)
 
-    ticker_list = [line.rstrip().decode("utf-8") for line in file.readlines()]
+    ticker_list = [line.rstrip().decode("utf-8").split()[0] for line in inp_file.readlines()]
+    inp_file.close()
 
     page_list = getPages(ticker_list)
-    list(map(getData, page_list))
+    #list(map(getData, page_list))
+
+    for page in page_list:
+        getData(page, out_file)
+
+    out_file.close()
+
 
     print("Runtime: ", time.time() - start_time, " s")
